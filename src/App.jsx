169daf93635108cc44452a99bf66838fab869502
devwqc <devwqc@gmail.com>
2024-02-25 22:59:18 +0900

    feat: 프로젝트 초기 폴더구조 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
-function App() {}
+import styles from './App.module.scss';
+
+function App() {
+  return <h1 className={styles.hello}>2+2=22</h1>;
+}
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,9 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  css: {
+    preprocessorOptions: {
+      scss: { additionalData: `@import "./src/styles/global.scss";` },
+    },
+  },
 });

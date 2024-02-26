3630437521d355f84fead2c014a34720efc2c35b
Chaemin-153 <0520032@gmail.com>
2024-02-26 16:20:29 +0900

    feat: Header Logo 이미지 링크 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,15 @@
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
+
 function Header() {
   return (
     <>
       <header className="header">
-        <div className="header-logo"></div>
+        <div className="header-logo">
+          <Link to="/">
+            <img src={logo} alt="logo"></img>
+          </Link>
+        </div>
         <div className="header-btn">롤링 페이퍼 만들기</div>
       </header>
     </>

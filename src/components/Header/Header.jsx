50ce0a04613a3d2cd930b51b9f83c8f5378cc03b
Chaemin-153 <0520032@gmail.com>
2024-02-26 15:48:03 +0900

    feat: Header 컴포넌트 파일 추가

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <>
+      <header className="header">
+        <div className="header-logo"></div>
+        <div className="header-btn">롤링 페이퍼 만들기</div>
+      </header>
+    </>
+  );
+}
+
+export default Header;

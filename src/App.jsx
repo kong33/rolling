f00afce076ac1b2c45d20839664525d35b5552ea
Chaemin-153 <0520032@gmail.com>
2024-02-27 22:48:29 +0900

    feat: 중첩 Router & Layout 추가(코드 리뷰 적용)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import LandingPage from './pages/landingpage/index.jsx';
-import ListPage from './pages/listpage/index.jsx';
-import PostPage from './pages/postpage/index.jsx';
+import Layout from './pages/layout';
+import LandingPage from './pages/landingpage';
+import ListPage from './pages/listpage';
+import PostPage from './pages/postpage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/list" element={<ListPage />} />
-        <Route path="/post" element={<PostPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<LandingPage />} />
+          <Route path="list" element={<ListPage />} />
+          <Route path="post" element={<PostPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/landingpage/index.jsx
+++ src/pages/landingpage/index.jsx
@@ -1,9 +1,7 @@
-import Header from '../../components/Header/Header.jsx';
-
 function LandingPage() {
   return (
     <>
-      <Header />
+      <h1>Landing Page!</h1>
     </>
   );
 }

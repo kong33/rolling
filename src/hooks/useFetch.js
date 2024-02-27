1a51385e4f9edf60c63e67b9ddbc1dc3b79f1790
김보경 <rlaqhrud6263@naver.com>
2024-02-27 16:46:17 +0900

    refactor: useFetch 훅 ver1 제작완료

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,4 +1,4 @@
-const baseURL = 'https://rolling-api.vercel.app';
+import { useState, useEffect } from 'react';
 
 // 1. 우선 fetch 기본 함수 제작 후
 // 2. { get put patch delete post } Hooks를 추가적으로 제작
@@ -6,23 +6,41 @@ const baseURL = 'https://rolling-api.vercel.app';
 // 4. url 가공 필요
 
 // 기본 fetch에서 url, method, body을 받아 실행
-async function uesFetch(url, method = 'GET', body) {
-  try {
-    const response = await fetch(`${baseURL}${url}`, {
-      method: method,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    });
-    return response;
-  } catch (error) {
-    console.log(`서버와의 통신 과정에서 문제가 발생하였습니다. (fetch 확인)`);
-    console.error(error);
-  }
-}
+const baseURL = 'https://rolling-api.vercel.app';
+const useFetch = (url, method = 'GET', body) => {
+  const [isData, setIsData] = useState(null);
 
-export default uesFetch;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${baseURL}${url}`, {
+          method: method,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(body),
+        });
+        if (!response.ok) {
+          throw new Error('서버 응답 실패');
+        }
+        const result = response.json();
+        setIsData(result);
+      } catch (error) {
+        console.error(error);
+        console.log(
+          `서버와의 통신 과정에서 문제가 발생하였습니다. (fetch 확인)`,
+        );
+      }
+    };
+
+    fetchData();
+  }, [url, method, body]);
+  // url, method, body가 변경될 때마다 호출
+
+  return isData;
+};
+
+export default useFetch;
 
 // const API = {
 //   // GET: 리소스 조회

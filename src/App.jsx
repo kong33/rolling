79690d349be13e37814fcc412fb59a97b5e17c77
Chaemin-153 <0520032@gmail.com>
2024-02-27 22:28:18 +0900

    style: Header css 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import styles from './App.module.scss';
 import LandingPage from './pages/LandingPage.jsx';
 
 function App() {
   return (
     <>
       <LandingPage />
-      <h1 className={styles.hello}>2+2=22</h1>
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,23 @@ import styles from './Header.module.scss';
 function Header() {
   return (
     <header>
-      <div>
-        <img src={logo} alt="logo"></img>
-      </div>
-      <div className={styles.btn}>롤링 페이퍼 만들기</div>
+      <nav-top>
+        <div>
+          <img src={logo} alt="logo"></img>
+        </div>
+        <div className={styles.btn}>롤링 페이퍼 만들기</div>
+      </nav-top>
+      <nav-bottom>
+        <section>
+          <div className={styles.toName}>To. Ashley Kim</div>
+        </section>
+        <section>
+          <div className={styles.postNumbers}>23명이 작성했어요!</div>
+          <div className={styles.emoziBtns}>이모지s</div>
+          <div className={`${styles.addEmoziBtn} ${styles.btn}`}>:) 추가</div>
+          <div className={`${styles.shareBtn} ${styles.btn}`}>sharing</div>
+        </section>
+      </nav-bottom>
     </header>
   );
 }

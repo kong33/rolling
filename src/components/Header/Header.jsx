790e10bf3f1652dcbe8d29e2347a562a06622225
Chaemin-153 <0520032@gmail.com>
2024-02-27 12:09:17 +0900

    feat: Header css 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,14 @@
 import logo from '../../assets/images/logo.png';
+import styles from './Header.module.scss';
 
 function Header() {
   return (
-    <>
-      <header className="header">
-        <div className="header-logo">
-          <img src={logo} alt="logo"></img>
-        </div>
-        <div className="header-btn">롤링 페이퍼 만들기</div>
-      </header>
-    </>
+    <header>
+      <div>
+        <img src={logo} alt="logo"></img>
+      </div>
+      <div className={styles.btn}>롤링 페이퍼 만들기</div>
+    </header>
   );
 }
 

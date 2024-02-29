47f305d51ccec0756641d8a6ad2610c64d250289
devwqc <devwqc@gmail.com>
2024-02-29 19:32:45 +0900

    refactor: Button styleType normal -> outlined 변경

--- src/components/Button/Button/Button.jsx
+++ src/components/Button/Button/Button.jsx
@@ -2,15 +2,15 @@ import { BUTTON_SIZE, BUTTON_TYPE } from '../../../constants/button';
 import styles from './Button.module.scss';
 
 const STYLE_TYPE = {
-  normal: 'normal',
   primary: 'primary',
   secondary: 'secondary',
+  outlined: 'outlined',
 };
 
 function Button({
   children,
   type = BUTTON_TYPE.button,
-  styleType = STYLE_TYPE.normal,
+  styleType = STYLE_TYPE.primary,
   size = BUTTON_SIZE.sm,
   onClick,
   className = '',
@@ -18,7 +18,7 @@ function Button({
 }) {
   const buttonType = BUTTON_TYPE[type] || BUTTON_TYPE.button;
   const buttonSize = BUTTON_SIZE[size] || BUTTON_SIZE.sm;
-  const pStyleType = STYLE_TYPE[styleType] || STYLE_TYPE.normal;
+  const pStyleType = STYLE_TYPE[styleType] || STYLE_TYPE.primary;
   const classNames = `${styles.button} ${styles[buttonSize]} ${styles[pStyleType]} ${className}`;
 
   const handleClick = onClick

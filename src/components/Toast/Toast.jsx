61e2c66b145ccb2f56896f1ee7eacf74888463fe
Chaemin-153 <0520032@gmail.com>
2024-03-01 11:51:32 +0900

    feat: URL 공유시 Toast 5초간 뜨다가 없어짐 추가

--- src/components/Toast/Toast.jsx
+++ src/components/Toast/Toast.jsx
@@ -1,14 +1,24 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import close from '../../assets/images/close.svg';
 import completed from '../../assets/images/completed.svg';
 import styles from './Toast.module.scss';
 
-function Toast() {
+function Toast({ setToast }) {
   const toastRef = useRef();
 
   const handleQuitToast = () => {
-    toastRef.current.style.display = 'none';
+    // toastRef.current.style.display = 'none';
+    setToast(false);
   };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setToast(false);
+    }, 5000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [setToast]);
 
   return (
     <div className={styles.toast} ref={toastRef}>

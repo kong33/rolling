213ae0646f244c4636eade2cef782d266693ab78
김보경 <rlaqhrud6263@naver.com>
2024-03-01 12:18:35 +0900

    refactor: CardOverview 컴포넌트 작업 완료

--- src/components/CardList/CardOverview.jsx
+++ src/components/CardList/CardOverview.jsx
@@ -8,6 +8,7 @@ const CardOverview = ({ recipient }) => {
   const data = useFetch(`/2-7/recipients/${recipient.id}/`).recentMessages;
 
   useEffect(() => {
+    // 데이터 로딩이 완료되면 senders 배열로 업데이트
     if (data) {
       setSenders(data);
     }
@@ -15,18 +16,19 @@ const CardOverview = ({ recipient }) => {
 
   useEffect(() => {
     if (senders.length > 0) {
-      // senders 배열이 업데이트되었을 때 senders를 createdAt 기준으로 정렬합니다.
+      // senders 배열이 업데이트되었을 때 senders를 createdAt 기준으로 정렬
       const sortedSenders = [...senders].sort(
         (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
       );
 
-      // 상위 3개의 sender를 선택합니다.
-      const senderPreview = sortedSenders.slice(0, 3);
+      const senderPreview = sortedSenders.filter((item, i) => i < 3 && item);
 
       // 선택된 sender를 상태에 설정합니다.
       setSenders(senderPreview);
     }
-  }, [senders]);
+    // 아래 dependency값에서 에러가나는데 어떤값을 넣어야할지 모르겠습니다.
+    // sender를 넣으면 무한호출됨
+  }, []);
 
   return (
     <article className={styles.content}>
@@ -40,6 +42,8 @@ const CardOverview = ({ recipient }) => {
               src={item.profileImageURL}
             />
           ))}
+          {/* 표현된 profileImege를 제외한 나머지 인원을 count로 추가 렌더링 필요 */}
+          {/* profileImageContainer의 요소들이 reative하게 16xp 간격으로 곂쳐진 style 추가 필요 */}
         </div>
         <p className={styles.text}>
           <span className={styles.count}>{recipient.messageCount}명</span>이

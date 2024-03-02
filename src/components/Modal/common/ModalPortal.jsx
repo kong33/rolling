77e804a9e465bd8704c454437450fb102c9e2361
devwqc <devwqc@gmail.com>
2024-03-02 18:20:49 +0900

    remove: Modal Context 관련 삭제

--- src/components/Modal/common/ModalPortal.jsx
+++
@@ -1,8 +0,0 @@
-import { createPortal } from 'react-dom';
-
-function ModalPortal({ children }) {
-  const modalRoot = document.getElementById('modal');
-  return createPortal(children, modalRoot);
-}
-
-export default ModalPortal;

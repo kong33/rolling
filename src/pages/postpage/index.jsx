e00b2eec7d72aff113fb5c977d54f9df21129cbc
kong33 <kge021114@naver.com>
2024-03-03 16:55:02 +0900

    refactor : texteditor 안 쓰는 파일 및 코드 정리

--- src/pages/postpage/index.jsx
+++ src/pages/postpage/index.jsx
@@ -1,12 +1,13 @@
 import Dropdown from '../../components/Dropdown/Dropdown';
 import Input from '../../components/Input/Input';
-
+import ReactDraft from '../../components/ReactDraft/ReactDraft';
 function PostPage() {
   let placeholder = [1, 2, 3];
   return (
     <>
       <Dropdown label="친구" name="ahdi" options={placeholder} />
       <Input placeholder="적어라" errorMassage="에러" />
+      <ReactDraft />
     </>
   );
 }

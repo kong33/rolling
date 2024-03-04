9e8da2cff12a9215ca4e457c666bbec9040060b7
kong33 <kge021114@naver.com>
2024-03-04 12:10:41 +0900

    feat : input label 추가

--- src/pages/postpage/index.jsx
+++ src/pages/postpage/index.jsx
@@ -3,12 +3,19 @@ import Option from '../../components/Option/Option';
 import styles from './Post.module.scss';
 //import Layout from '../layout';
 function PostPage() {
+  const LABEL = '배경화면을 선택해 주세요';
+  const DESCRIPTION = '컬러를 선택하거나, 이미지를 선택할 수 있습니다.';
   return (
     // <Layout>
+
     <div className={styles.div}>
       <Input placeholder="적어라" errorMassage="에러" label="To." />
+      <h1 className={styles.h1}>{LABEL}</h1>
+      <p className={styles.p}>{DESCRIPTION}</p>
+      {/* 버튼 */}
       <Option type="color" />
     </div>
+
     //Layout>
   );
 }

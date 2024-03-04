cd82c5b28a818f2d6f70a7d3d9622a18fb81698d
kong33 <kge021114@naver.com>
2024-03-04 14:47:44 +0900

    feat : 머지를 위해 커밋

--- src/pages/postpage/index.jsx
+++ src/pages/postpage/index.jsx
@@ -7,15 +7,15 @@ function PostPage() {
   const DESCRIPTION = '컬러를 선택하거나, 이미지를 선택할 수 있습니다.';
   return (
     // <Layout>
-
-    <div className={styles.div}>
-      <Input placeholder="적어라" errorMassage="에러" label="To." />
-      <h1 className={styles.h1}>{LABEL}</h1>
-      <p className={styles.p}>{DESCRIPTION}</p>
-      {/* 버튼 */}
-      <Option type="color" />
-    </div>
-
+    <form>
+      <div className={styles.div}>
+        <Input placeholder="적어라" errorMassage="에러" label="To." />
+        <h1 className={styles.h1}>{LABEL}</h1>
+        <p className={styles.p}>{DESCRIPTION}</p>
+        {/* 버튼 */}
+        <Option type="color" />
+      </div>
+    </form>
     //Layout>
   );
 }

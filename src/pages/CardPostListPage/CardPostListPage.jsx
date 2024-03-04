01a0b0d2d5d5661557f8e460aa2a13e987e616f0
jinah-dev <kimjinah.dev@gmail.com>
2024-03-04 16:32:30 +0900

    Feat : CardPostListPage 기초구축

---
+++ src/pages/CardPostListPage/CardPostListPage.jsx
@@ -0,0 +1,11 @@
+import styles from './CardPostListPage.module.scss';
+
+function CardPostListPage() {
+  return (
+    <>
+      <h1 className={styles.container}>CardPostListPage</h1>
+    </>
+  );
+}
+
+export default CardPostListPage;

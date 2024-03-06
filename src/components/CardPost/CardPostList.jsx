572df8b399a98b6df8edd7c990cdd1f8c93feb9a
jinah-dev <kimjinah.dev@gmail.com>
2024-03-06 17:07:34 +0900

    Feat : CardPostList에 onDelete props CardPost로 넘겨줌
    Co-authored-by: devwqc <devwqc@gmail.com>

--- src/components/CardPost/CardPostList.jsx
+++ src/components/CardPost/CardPostList.jsx
@@ -1,12 +1,12 @@
 import style from './CardPostList.module.scss';
 import CardPost from './CardPost';
 
-function CardPostList({ items }) {
+function CardPostList({ items, onDelete }) {
   return (
     <div className={style.cardContainer}>
       <div className={style.cardList}>
         {items?.map((item) => (
-          <CardPost key={item.id} item={item} /> //빨간색 에러 해결
+          <CardPost key={item.id} item={item} onDelete={onDelete} /> //빨간색 에러 해결
         ))}
       </div>
     </div>

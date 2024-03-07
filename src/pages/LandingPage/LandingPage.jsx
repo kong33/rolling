27204700c4352d05ce2dbb97a31a1f2b9b207641
Chaemin-153 <0520032@gmail.com>
2024-03-07 09:42:56 +0900

    refactor: div -> p 태그명 수정

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -6,46 +6,44 @@ import styles from './Landing.module.scss';
 
 function LandingPage() {
   return (
-    <>
-      <main>
-        {/* 상단 Box */}
-        <div className={styles.pointBox}>
-          {/* 상단 - Left */}
-          <div className={styles.textBox}>
-            <div className={styles.pointBtn}>Point. 01</div>
-            <div className={styles.boldText}>
-              누구나 손쉽게, 온라인 <br />
-              롤링 페이퍼를 만들 수 있어요
-            </div>
-            <div>로그인 없이 자유롭게 만들어요.</div>
-          </div>
-          {/* 상단 - Right */}
-          <div className={styles.imgBox}>
-            <img src={point1Img} alt="rolling card" />
-          </div>
+    <main>
+      {/* 상단 Box */}
+      <div className={styles.pointBox}>
+        {/* 상단 - Left */}
+        <div className={styles.textBox}>
+          <div className={styles.pointBtn}>Point. 01</div>
+          <p className={styles.boldText}>
+            누구나 손쉽게, 온라인 <br />
+            롤링 페이퍼를 만들 수 있어요
+          </p>
+          <p>로그인 없이 자유롭게 만들어요.</p>
         </div>
-        {/* 하단 Box */}
-        <div className={styles.pointBox}>
-          {/* 하단 - Left */}
-          <div className={styles.imgBox}>
-            <img src={point2Img} alt="emozi" />
-          </div>
-          {/* 하단 - Right */}
-          <div className={styles.textBox}>
-            <div className={styles.pointBtn}>Point. 02</div>
-            <div className={styles.boldText}>
-              서로에게 이모지로 감정을 표현해보세요
-            </div>
-            <div>롤링 페이퍼에 이모지를 추가할 수 있어요.</div>
-          </div>
+        {/* 상단 - Right */}
+        <div className={styles.imgBox}>
+          <img src={point1Img} alt="rolling card" />
         </div>
-        <Link to="/list">
-          <Button styleType="primary56" size="md" className={styles.btn}>
-            구경해보기
-          </Button>
-        </Link>
-      </main>
-    </>
+      </div>
+      {/* 하단 Box */}
+      <div className={styles.pointBox}>
+        {/* 하단 - Left */}
+        <div className={styles.imgBox}>
+          <img src={point2Img} alt="emozi" />
+        </div>
+        {/* 하단 - Right */}
+        <div className={styles.textBox}>
+          <div className={styles.pointBtn}>Point. 02</div>
+          <p className={styles.boldText}>
+            서로에게 이모지로 감정을 표현해보세요
+          </p>
+          <p>롤링 페이퍼에 이모지를 추가할 수 있어요.</p>
+        </div>
+      </div>
+      <Link to="/list">
+        <Button styleType="primary56" size="md" className={styles.btn}>
+          구경해보기
+        </Button>
+      </Link>
+    </main>
   );
 }
 

fe2ea376c76524f974b64690928e05a2041f9078
jinah-dev <kimjinah.dev@gmail.com>
2024-03-07 21:16:13 +0900

    Refactor : CardPostList에 onAdd 버튼 연결
    Co-authored-by: devwqc <devwqc@gmail.com>

--- src/components/CardPost/CardPostList.jsx
+++ src/components/CardPost/CardPostList.jsx
@@ -2,11 +2,11 @@ import styles from './CardPostList.module.scss';
 import CardPlus from './CardPlus';
 import CardPost from './CardPost';
 
-function CardPostList({ items, onDelete }) {
+function CardPostList({ items, onDelete, onAdd }) {
   return (
     <div className={styles.cardContainer}>
       <div className={styles.cardList}>
-        {onDelete || <CardPlus />}
+        {onDelete || <CardPlus onAdd={onAdd} />}
         {items?.map((item) => (
           <CardPost key={item.id} item={item} onDelete={onDelete} /> //빨간색 에러 해결
         ))}

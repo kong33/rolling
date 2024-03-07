76411e5ce0c3e05fa7dab226f372731d62dfe923
JinahKim <kimjinah.dev@gmail.com>
2024-03-07 18:17:28 +0900

    Feat : CardPostList에 CardPlus 추가연결

--- src/components/CardPost/CardPostList.jsx
+++ src/components/CardPost/CardPostList.jsx
@@ -1,10 +1,12 @@
 import styles from './CardPostList.module.scss';
+import CardPlus from './CardPlus';
 import CardPost from './CardPost';
 
 function CardPostList({ items, onDelete }) {
   return (
     <div className={styles.cardContainer}>
       <div className={styles.cardList}>
+        <CardPlus />
         {items?.map((item) => (
           <CardPost key={item.id} item={item} onDelete={onDelete} /> //빨간색 에러 해결
         ))}

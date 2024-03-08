07249de1eb0afe649c6669ea37495b46c41c987f
jinah-dev <kimjinah.dev@gmail.com>
2024-03-08 15:28:38 +0900

    Feat : CardPostList에 onClick prop 추가
    Co-authored-by: devwqc <devwqc@gmail.com>

--- src/components/CardPost/CardPostList.jsx
+++ src/components/CardPost/CardPostList.jsx
@@ -2,13 +2,18 @@ import styles from './CardPostList.module.scss';
 import CardPlus from './CardPlus';
 import CardPost from './CardPost';
 
-function CardPostList({ items, onDelete, onAdd }) {
+function CardPostList({ items, onDelete, onAdd, onClick }) {
   return (
     <div className={styles.cardContainer}>
       <div className={styles.cardList}>
         {onAdd && <CardPlus onAdd={onAdd} />}
         {items?.map((item) => (
-          <CardPost key={item.id} item={item} onDelete={onDelete} /> //빨간색 에러 해결
+          <CardPost
+            key={item.id}
+            item={item}
+            onDelete={onDelete}
+            onClick={onClick}
+          />
         ))}
       </div>
     </div>

c93dd12e702682e736e861e31defb5160665dd66
kong33 <kge021114@naver.com>
2024-03-11 15:59:41 +0900

    feat : errorpage에 button 추가 및 css 조정

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,14 @@
 import errorImage from '../../assets/images/errorPage.png';
+import { Button } from '../../components/Button';
+import { Link } from 'react-router-dom';
+import styles from './ErrorPage.module.scss';
 export default function ErrorPage() {
   return (
-    <div>
-      <img src={errorImage} alt="에러페이지" />
+    <div className={styles.div}>
+      <img src={errorImage} alt="에러페이지" className={styles.img} />
+      <Link to="/">
+        <Button size="md">돌아가기</Button>
+      </Link>
     </div>
   );
 }

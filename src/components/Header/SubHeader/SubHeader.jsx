77b243b62dbc05cdbf6bff2d2a81d6d55c719952
Chaemin-153 <0520032@gmail.com>
2024-03-11 20:21:03 +0900

    style: Header 반응형 적용중

--- src/components/Header/SubHeader/SubHeader.jsx
+++ src/components/Header/SubHeader/SubHeader.jsx
@@ -50,75 +50,73 @@ export default function SubHeader() {
   };
 
   return (
-    <header>
-      <nav className={styles.bottomNav}>
-        {/* To. 000 */}
-        <section className={styles.section}>
-          <div className={styles.toName}>To. {name}</div>
-        </section>
-        <section className={styles.section}>
-          {/* 작성자 프로필 사진 */}
-          <div
-            className={`${styles.profileImageContainer} ${styles[`imageCount${recentMessages.length}`]}`}
-          >
-            {recentMessages.map((sender, i) => (
-              <img
-                className={`${styles[`visitorImage-${i + 1}`]}`}
-                key={sender.id}
-                src={sender.profileImageURL}
-              />
-            ))}
-            {messageCount - 3 > 0 ? (
-              <div className={styles.visitorCount}>+{messageCount - 3}</div>
-            ) : (
-              <div></div>
-            )}
-          </div>
-          {/* 00명이 작성했어요 */}
-          <div className={styles.postNumbers}>
-            <span className={styles.countBold}>{messageCount}</span>
-            명이 작성했어요!
-          </div>
-          <div className={styles.line}></div>
-          {/* 이모지 상위 3개 보여주기 */}
-          <Reactions reactions={topReactions} />
-          {/* 이모지 더 보기 버튼 */}
-          {topReactions.length ? (
-            emojiBoxIsOpen ? (
-              <div>
-                <ArrowUp />
-              </div>
-            ) : (
-              <div className={styles.toggleBtn} onClick={openEmojiToggle}>
-                <ArrowDown />
-              </div>
-            )
+    <nav className={styles.bottomNav}>
+      {/* To. 000 */}
+      <section className={styles.section}>
+        <div className={styles.toName}>To. {name}</div>
+      </section>
+      <section className={styles.section}>
+        {/* 작성자 프로필 사진 */}
+        <div
+          className={`${styles.profileImageContainer} ${styles[`imageCount${recentMessages.length}`]}`}
+        >
+          {recentMessages.map((sender, i) => (
+            <img
+              className={`${styles[`visitorImage-${i + 1}`]}`}
+              key={sender.id}
+              src={sender.profileImageURL}
+            />
+          ))}
+          {messageCount - 3 > 0 ? (
+            <div className={styles.visitorCount}>+{messageCount - 3}</div>
           ) : (
             <div></div>
           )}
-          {/* 이모지 토글 박스 */}
-          <ShowEmojiBox
-            team={TEAM}
-            recipientId={recipientId}
-            close={closeEmojiToggle}
-            isOpen={emojiBoxIsOpen}
-          />
-          {/* 이모지 추가 버튼 */}
-          <AddEmojiBtn
-            isOpen={emojiPickerIsOpen}
-            setIsOpen={setEmojiPickerIsOpen}
-          />
-          {/* 이모지 추가 토글 박스 */}
-          <AddEmojiBox
-            isOpen={emojiPickerIsOpen}
-            setIsOpen={setEmojiPickerIsOpen}
-            setIsSelected={setEmojiIsSelected}
-          />
-          <div className={styles.line}></div>
-          {/* 공유 토글 박스 */}
-          <ShareBtn />
-        </section>
-      </nav>
-    </header>
+        </div>
+        {/* 00명이 작성했어요 */}
+        <div className={styles.postNumbers}>
+          <span className={styles.countBold}>{messageCount}</span>
+          명이 작성했어요!
+        </div>
+        <div className={styles.line}></div>
+        {/* 이모지 상위 3개 보여주기 */}
+        <Reactions reactions={topReactions} />
+        {/* 이모지 더 보기 버튼 */}
+        {topReactions.length ? (
+          emojiBoxIsOpen ? (
+            <div>
+              <ArrowUp />
+            </div>
+          ) : (
+            <div className={styles.toggleBtn} onClick={openEmojiToggle}>
+              <ArrowDown />
+            </div>
+          )
+        ) : (
+          <div></div>
+        )}
+        {/* 이모지 토글 박스 */}
+        <ShowEmojiBox
+          team={TEAM}
+          recipientId={recipientId}
+          close={closeEmojiToggle}
+          isOpen={emojiBoxIsOpen}
+        />
+        {/* 이모지 추가 버튼 */}
+        <AddEmojiBtn
+          isOpen={emojiPickerIsOpen}
+          setIsOpen={setEmojiPickerIsOpen}
+        />
+        {/* 이모지 추가 토글 박스 */}
+        <AddEmojiBox
+          isOpen={emojiPickerIsOpen}
+          setIsOpen={setEmojiPickerIsOpen}
+          setIsSelected={setEmojiIsSelected}
+        />
+        <div className={styles.line}></div>
+        {/* 공유 토글 박스 */}
+        <ShareBtn />
+      </section>
+    </nav>
   );
 }
